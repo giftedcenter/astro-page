0ed2be1a4e47b26df929c06b09497824a9001d67
conquestace <ashiful@conquestace.com>
2025-06-04 17:02:20 -0400

    big commit with ai-literacy and coding camp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
 
-// https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  integrations: [
+    mdx(),
+    icon({
+      customCollections: {
+        local: './src/icons', // allow usage of <Icon name="local:scales" />
+      },
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
 })
